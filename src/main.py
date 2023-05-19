4bf5960dc298659c9c44d1cdb85e8fff21f29ee0
joao guilherme melo <jgocm@cin.ufpe.br>
2023-05-19 13:24:48 -0300

    computes kick discharge time (ms)

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ plt.xlabel('Distance (m)')
 plt.ylabel('Speed (m/s)')
 plt.title('Ball Kicks')
 plt.legend()
-plt.show()
+#plt.show()
 
 # TODO: quebrar o problema em duas partes oara resolver a regressão:
 # 1-baseado na velocidade máxima, qual a trajetória?
@@ -84,7 +84,7 @@ for tracker in trackers[2:]:
 plt.xlabel('Distance (m)')
 plt.ylabel('Speed (m/s)')
 plt.title('Ball Kicks')
-plt.show()
+#plt.show()
 
 
 # Find B from max_speed
@@ -95,7 +95,9 @@ X = np.array(max_speeds).reshape(-1, 1)
 y = np.array(Bs).reshape(-1, 1)
 model.fit(X, y)
 y_predict = model.estimator_.coef_[0]*X+model.estimator_.intercept_
-print(f'A: {np.mean(As)} , ab: {model.estimator_.coef_[0]}, bb: {model.estimator_.intercept_}')
+Av = np.mean(As)
+Ab = model.estimator_.coef_[0]
+Bb = model.estimator_.intercept_
 
 plt.scatter(max_speeds, y, label='original')
 plt.scatter(max_speeds, y_predict, label='regression')
@@ -103,45 +105,103 @@ plt.xlabel('Max Speed (m/s)')
 plt.ylabel('B')
 plt.title('Ball Kicks')
 plt.legend()
-plt.show()
+#plt.show()
 plt.clf()
 
+# Plot results for testing
+for tracker in trackers[2:]:
+    Vmax = tracker.max_speed
+    d = tracker.distances
+    v = Av*d + Ab*Vmax + Bb
+    plt.plot(d, v)
+    plt.plot(moving_average(tracker.distances, 3), 
+             moving_average(tracker.speeds, 3), 
+             label=tracker.title)
 
-'''
-Coefficients can be expressed as:
-A = np.mean(As)
-B = model.estimator_.coef_[0]*max_speed+model.estimator_.intercept_
-'''
+plt.xlabel('Distance (m)')
+plt.ylabel('Speed (m/s)')
+plt.title('Ball Kicks')
+#plt.show()
 
-# Determining distance for achieving max_speed from the kick strength (discharge time in ms)
+# Determining max_speed from the kick strength (discharge time in ms)
 from scipy.optimize import curve_fit
-
+Fmin = 0.9
 def func(X, A, B):
-    return A * (1 - np.exp(-B * (X-1)))
+    return A * (1 - np.exp(-B * (X-Fmin)))
 
 As = []
 Bs = []
 dists = []
 kick_strengths = []
+max_speeds = []
 for tracker in trackers:
     dists.append(tracker.distances[np.argmax(tracker.speeds)])
     kick_strengths.append(tracker.kick_strength)
+    max_speeds.append(tracker.max_speed)
 
 X = np.array(kick_strengths)
-y = np.array(dists)
+y = np.array(max_speeds)
 popt, pcov = curve_fit(func, X, y)
-A = popt[0]
-B = popt[1]
-y_predict = func(X, A, B)
+Ad = popt[0]
+Bd = popt[1]
+y_predict = func(X, Ad, Bd)
 
-plt.scatter(kick_strengths, dists, label='original')
+plt.scatter(kick_strengths, max_speeds, label='original')
 plt.scatter(kick_strengths, y_predict, label='regression')
 plt.xlim(0, 7)
 plt.ylim(0, 3.5)
 plt.xlabel('Discharge Time (ms)')
-plt.ylabel('Distance for Maximum Speed (m)')
+plt.ylabel('Maximum Speed (m/s)')
 plt.title('Ball Kicks')
 plt.legend()
-plt.show()
+#plt.show()
    
-# Plot results for testing
+for tracker in trackers[2:]:
+    distances = tracker.distances
+    speeds_measured = tracker.speeds
+    v_max_estimated = func(tracker.kick_strength, Ad, Bd)
+    speeds_estimated = Av*distances + Ab*v_max_estimated + Bb
+    plt.plot(distances, speeds_estimated)
+    plt.plot(distances, speeds_measured)
+
+    # Highlight max speed point
+    plt.plot(distances[np.argmax(tracker.speeds)], 
+             speeds_measured[np.argmax(tracker.speeds)], 
+             marker='o', 
+             markersize=3, 
+             color='red')
+    v_max_estimated_index = np.argmin(np.abs(speeds_estimated-v_max_estimated))
+    plt.plot(distances[v_max_estimated_index], 
+             speeds_estimated[v_max_estimated_index], 
+             marker='o', 
+             markersize=3, 
+             color='red')
+    
+plt.xlabel('Distance (m)')
+plt.ylabel('Speed (m/s)')
+plt.title('Ball Kicks')
+#plt.show()
+plt.clf()
+
+# Checking coefficients
+Ab = Ab[0]
+Bb = Bb[0]
+print(f'Av: {Av} , Ab: {Ab}, Bb: {Bb}, Ad: {Ad}, Bd: {Bd}, Fmin: {Fmin}')
+
+# Test final equation
+robot_distances = [1, 2, 3, 4, 5, 6, 7, 8]
+desired_ball_speed = 2.9
+Fs = []
+
+for d in robot_distances:
+    v = desired_ball_speed
+    gamma = (v - Av*d - Bb) / (Ab*Ad)
+    F = Fmin - np.log(1-gamma)/Bd
+    if F<Fmin : F = Fmin
+    Fs.append(F)
+    plt.scatter(d, F)
+
+plt.xlabel('Robot Distance (m)')
+plt.ylabel('Discharge Time (ms)')
+plt.title('Ball Kicks')
+plt.show()
